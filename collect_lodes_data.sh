89c7dbe88c9e81f5abbca3d7a12fa4245bd6e456
Stefan McCabe <stefanmccabe@gmail.com>
2020-03-15 18:27:11 -0400

    correct bug in collection script

--- collect_lodes_data.sh
+++ collect_lodes_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p "data/derived"
-cd data/derived
+mkdir -p "data/raw"
+cd data/raw
 wget -r -np --cut-dirs 2 -nH -N -A '*_JT00_2016.csv.gz,*_xwalk.csv.gz' -R 'us_xwalk.csv.gz' "https://lehd.ces.census.gov/data/lodes/LODES7/"
 cd ../..
 
